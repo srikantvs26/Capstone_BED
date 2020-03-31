326ac5db2057a686a563280c27d8065a1e78983a
srikant vs <srikantvs26@gmail.com>
2020-03-31 11:27:07 +0530

    import file

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,12 +4,14 @@ INSERT INTO Monthlymeasures(id,fname,email,firstname,secondname,thirdname,zone) 
 INSERT INTO Monthlymeasures(id,fname,email,firstname,secondname,thirdname,zone) VALUES(-4,'John','johndoe@gmail.com','null','null','null',26);
 INSERT INTO Monthlymeasures(id,fname,email,firstname,secondname,thirdname,zone) VALUES(-5,'Jane','janedoe@gmail.com','null','null','null',26);
 INSERT INTO Monthlymeasures(id,fname,email,firstname,secondname,thirdname,zone) VALUES(-6,'Ravi','ravikumar@gmail.com','null','null','null',26);
+
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('sumeets@gmail.com','Sumeet','Singh',25,'Male','7382456600','KR Puram','BLR','Karnataka','India',560047,190,89,'Not Applicable','Not Applicable','veg','johndoe','approved');
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('jonask@gmail.com','Jonas','K',29,'Male','9582186600','Ashok Nagar','BLR','Karnataka','India',560001,185,78,'Not Applicable','Not Applicable','veg','janedoe','approved');
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('marthan@gmail.com','Martha','N',25,'Female','9982146600','Mg Road','BLR','Karnataka','India',561254,176,79,'Not Applicable','Not Applicable','veg','janedoe','approved');
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('ravikumar@gmail.com','Ravi','Kumar',25,'Male','7382906600','Mg Road','BLR','Karnataka','India',587412,180,80,'Not Applicable','Not Applicable','veg','srikant','approved');
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('janedoe@gmail.com','Jane','Doe',25,'Female','7952146600','Lido','BLR','Karnataka','India',523698,172,70,'Not Applicable','Not Applicable','veg','srikant','approved');
 INSERT INTO Newuser(email,fname,lname,age,gender,mobile,address,city,state,country,pincode,height,weight,medical,dietary,food,referral,status) VALUES('johndoe@gmail.com','John','Doe',25,'Male','79821466630','Lido','BLR','Karnataka','India',541256,185,90,'Not Applicable','Not Applicable','veg','srikant','approved');
+
 INSERT INTO User(userid,fname,lname,email,password,type,division) VALUES('srikant','Srikant','V S','srikantvs26@gmail.com','admin@123','admin',0);
 INSERT INTO User(userid,fname,lname,email,password,type,division,doj) VALUES('johndoe','John','Doe','johndoe@gmail.com','user@123','user',26,'27/02/2020');
 INSERT INTO User(userid,fname,lname,email,password,type,division,doj) VALUES('janedoe','Jane','Doe','janedoe@gmail.com','user@123','user',26,'23/01/2020');
